cff5ade406dbce315409911d5963c17373a56b0d
Andy Trofimov <interlark@gmail.com>
2023-01-05 23:03:25 +0500

    Fix pyinstaller logging bug for builds

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ if __name__ == '__main__':
                 'PySimpleGUI==4.59.0',
             ],
             'dev': [
-                'pyinstaller>=5.0',
+                'pyinstaller>=5.0,<5.7.0',
                 'pytest>=6.2,<8',
                 'tox>=3.5,<4',
                 'pre-commit>=2.6',
